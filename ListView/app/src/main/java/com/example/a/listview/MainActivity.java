3976afb728f2873deadc7909601fb638ed57c8c8
Your Name <you@example.com>
2019-02-21 12:17:39 +0900

    -listview

---
+++ ListView/app/src/main/java/com/example/a/listview/MainActivity.java
@@ -0,0 +1,27 @@
+package com.example.a.listview;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+public class MainActivity extends AppCompatActivity {
+
+    String[] myData = {"hello", "world", "oracle", "java",
+            "hello", "world", "oracle", "java",
+            "hello", "world", "oracle", "java",
+            "hello", "world", "oracle", "java",
+    };
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        ListView listView = findViewById(R.id.listView);
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(
+                this, android.R.layout.simple_list_item_1 ,myData
+        );
+        listView.setAdapter(adapter);
+
+    }
+}
