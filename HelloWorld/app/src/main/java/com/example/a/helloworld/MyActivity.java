41460009cc5a5344b1c771aeae370b4992883ca1
Your Name <you@example.com>
2019-02-21 12:02:23 +0900

    activity

---
+++ HelloWorld/app/src/main/java/com/example/a/helloworld/MyActivity.java
@@ -0,0 +1,42 @@
+package com.example.a.helloworld;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+
+public class MyActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_my);
+
+        Button button = findViewById(R.id.myButton);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MyActivity.this, MainActivity.class);
+                intent.putExtra("value1", 1);
+//                startActivity(intent);
+                startActivityForResult(intent, 100);
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode == 100){
+            if(resultCode == RESULT_OK){
+                String myResult = data.getStringExtra("MyResult");
+                Toast.makeText(this, "result is "+myResult, Toast.LENGTH_SHORT).show();
+            }
+
+        }
+    }
+}
