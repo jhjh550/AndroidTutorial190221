4bef13c9d94697930d52df23dcc86cf7ed880011
Your Name <you@example.com>
2019-02-21 15:20:13 +0900

    - sqlite

--- SQLite/app/src/main/java/com/example/a/sqlite/TestSQLiteHandler.java
+++ SQLite/app/src/main/java/com/example/a/sqlite/TestSQLiteHandler.java
@@ -1,4 +1,37 @@
 package com.example.a.sqlite;
 
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+
 public class TestSQLiteHandler {
+    TestSQLiteOpenHelper helper;
+
+    public TestSQLiteHandler(Context context) {
+        helper = new TestSQLiteOpenHelper(context, "people", null, 1);
+    }
+
+    public void insert(String name, int age, String address){
+        SQLiteDatabase db = helper.getWritableDatabase();
+
+        ContentValues values = new ContentValues();
+        values.put("name", name);
+        values.put("age", age);
+        values.put("address", address);
+        db.insert("student", null, values);
+    }
+
+    public void delete(String name){
+        SQLiteDatabase db = helper.getWritableDatabase();
+        db.delete("student", "name = ?",
+                new String[]{name});
+    }
+
+    public void update(String name, int newAge){
+        SQLiteDatabase db = helper.getWritableDatabase();
+        ContentValues values = new ContentValues();
+        values.put("age", newAge);
+        db.update("student", values, "name = ?",
+                new String[]{name});
+    }
 }
