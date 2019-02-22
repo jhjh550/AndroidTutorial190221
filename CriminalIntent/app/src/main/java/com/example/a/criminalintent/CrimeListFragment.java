583d5cd902b81a42aa5bde0a78ef664b5d4f5351
Your Name <you@example.com>
2019-02-22 11:26:59 +0900

    init

--- CriminalIntent/app/src/main/java/com/example/a/criminalintent/CrimeListFragment.java
+++ CriminalIntent/app/src/main/java/com/example/a/criminalintent/CrimeListFragment.java
@@ -2,17 +2,25 @@ package com.example.a.criminalintent;
 
 
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.example.a.criminalintent.model.Crime;
+import com.example.a.criminalintent.model.CrimeLab;
+
+import java.util.List;
 
 
 public class CrimeListFragment extends Fragment {
 
     private RecyclerView mCrimeRecyclerView;
+    private CrimeAdapter mAdapter;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -22,7 +30,51 @@ public class CrimeListFragment extends Fragment {
         mCrimeRecyclerView = v.findViewById(R.id.crime_recycler_view);
         mCrimeRecyclerView.setLayoutManager(
                 new LinearLayoutManager(getActivity()) );
+        updateUI();
         return v;
+    }
+
+    private void updateUI(){
+        CrimeLab crimeLab = CrimeLab.get(getActivity());
+        List<Crime> crimes = crimeLab.getCrimes();
+        mAdapter = new CrimeAdapter(crimes);
+        mCrimeRecyclerView.setAdapter(mAdapter);
+
+    }
+
+    private class CrimeHolder extends RecyclerView.ViewHolder{
+        public TextView mTextView;
+        public CrimeHolder(@NonNull View itemView) {
+            super(itemView);
+            mTextView = (TextView) itemView;
+        }
+    }
+
+    private class CrimeAdapter extends RecyclerView.Adapter<CrimeHolder>{
+        private List<Crime> mCrimes;
+
+        public CrimeAdapter(List<Crime> crimes) {
+            mCrimes = crimes;
+        }
+
+        @NonNull
+        @Override
+        public CrimeHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
+            LayoutInflater inf = LayoutInflater.from(getActivity());
+            View view = inf.inflate(android.R.layout.simple_list_item_1, viewGroup, false);
+            return new CrimeHolder(view);
+        }
+
+        @Override
+        public void onBindViewHolder(@NonNull CrimeHolder crimeHolder, int i) {
+            Crime crime = mCrimes.get(i);
+            crimeHolder.mTextView.setText(crime.getTitle());
+        }
+
+        @Override
+        public int getItemCount() {
+            return mCrimes.size();
+        }
     }
 
 }
