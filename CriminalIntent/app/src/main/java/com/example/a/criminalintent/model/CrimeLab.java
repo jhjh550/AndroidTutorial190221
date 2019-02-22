2325ee0c686459ec3102a6ba27b869b0f6b7c744
Your Name <you@example.com>
2019-02-22 16:55:41 +0900

    init

--- CriminalIntent/app/src/main/java/com/example/a/criminalintent/model/CrimeLab.java
+++ CriminalIntent/app/src/main/java/com/example/a/criminalintent/model/CrimeLab.java
@@ -5,6 +5,9 @@ import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 
 import com.example.a.criminalintent.database.CrimeBaseHelper;
+import com.example.a.criminalintent.database.CrimeDbSchema;
+import com.example.a.criminalintent.database.CrimeDbSchema.CrimeTable;
+import com.example.a.criminalintent.database.CrimeDbSchema.CrimeTable.Cols;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -13,19 +16,14 @@ import java.util.UUID;
 
 public class CrimeLab {
     private static CrimeLab sCrimeLab;
-    private List<Crime> mCrimes;
+
     private SQLiteDatabase mDatabase;
     private Context mContext;
 
     public List<Crime> getCrimes() {
-        return mCrimes;
+        return new ArrayList<>();
     }
     public Crime getCrime(UUID id){
-        for(Crime crime: mCrimes){
-            if(crime.getId().equals(id)){
-                return crime;
-            }
-        }
         return null;
     }
 
@@ -37,13 +35,28 @@ public class CrimeLab {
     }
 
     public void addCrime(Crime c){
-        mCrimes.add(c);
+        ContentValues values = getContentValues(c);
+        mDatabase.insert(CrimeTable.NAME, null, values);
+    }
+    public void updateCrime(Crime c){
+        ContentValues values = getContentValues(c);
+        mDatabase.update(CrimeTable.NAME, values,
+                Cols.UUID + " = ?",
+                new String[]{ c.getId().toString() });
     }
 
     private CrimeLab(Context context){
-        mCrimes = new ArrayList<>();
         mContext = context;
         mDatabase = new CrimeBaseHelper(context).getWritableDatabase();
+    }
+
+    private static ContentValues getContentValues(Crime crime){
+        ContentValues values = new ContentValues();
+        values.put(Cols.UUID, crime.getId().toString());
+        values.put(Cols.TITLE, crime.getTitle());
+        values.put(Cols.DATE, crime.getDate().getTime());
+        values.put(Cols.SOLVED, crime.isSolved()? 1:0);
+        return values;
     }
 
 }
