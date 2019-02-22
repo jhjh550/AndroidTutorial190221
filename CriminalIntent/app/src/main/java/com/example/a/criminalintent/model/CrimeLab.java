075fdb3e2ab3fa77d9c76c9570d39896c34ce062
Your Name <you@example.com>
2019-02-22 10:41:17 +0900

    init

---
+++ CriminalIntent/app/src/main/java/com/example/a/criminalintent/model/CrimeLab.java
@@ -0,0 +1,37 @@
+package com.example.a.criminalintent.model;
+
+import android.content.ContentValues;
+import android.content.Context;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
+public class CrimeLab {
+    private static CrimeLab sCrimeLab;
+    private List<Crime> mCrimes;
+
+    public List<Crime> getCrimes() {
+        return mCrimes;
+    }
+    public Crime getCrime(UUID id){
+        for(Crime crime: mCrimes){
+            if(crime.getId().equals(id)){
+                return crime;
+            }
+        }
+        return null;
+    }
+
+    public static CrimeLab get(Context context){
+        if(sCrimeLab == null){
+            sCrimeLab = new CrimeLab(context);
+        }
+        return sCrimeLab;
+    }
+
+    private CrimeLab(Context context){
+        mCrimes = new ArrayList<>();
+    }
+
+}
