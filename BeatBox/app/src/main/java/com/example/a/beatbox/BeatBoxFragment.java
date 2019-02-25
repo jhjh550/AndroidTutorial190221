8c650e81b125fcefcf2394fc8aa58859dd39ab94
Your Name <you@example.com>
2019-02-25 14:23:47 +0900

    init

---
+++ BeatBox/app/src/main/java/com/example/a/beatbox/BeatBoxFragment.java
@@ -0,0 +1,57 @@
+package com.example.a.beatbox;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+public class BeatBoxFragment extends Fragment {
+    public static BeatBoxFragment newInstance() {
+        return new BeatBoxFragment();
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.fragment_beat_box, container, false);
+        RecyclerView recyclerView = v.findViewById(R.id.fragment_beat_box_recycler_view);
+        recyclerView.setLayoutManager(
+                new GridLayoutManager(getActivity(), 3));
+        return v;
+    }
+
+    private class SoundHolder extends RecyclerView.ViewHolder{
+        private Button mButton;
+
+        public SoundHolder(@NonNull View itemView) {
+            super(itemView);
+            mButton = (Button) itemView;
+        }
+    }
+
+    private class SoundAdapter extends RecyclerView.Adapter<SoundHolder>{
+
+        @NonNull
+        @Override
+        public SoundHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
+            return new SoundHolder();
+        }
+
+        @Override
+        public void onBindViewHolder(@NonNull SoundHolder soundHolder, int i) {
+
+        }
+
+        @Override
+        public int getItemCount() {
+            return 0;
+        }
+    }
+
+}
