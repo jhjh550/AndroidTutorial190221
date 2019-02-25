6746ee3874d2473b81e305d2ea3f3357d77f8847
Your Name <you@example.com>
2019-02-25 17:09:35 +0900

    init

---
+++ BeatBox_MVVM/app/src/main/java/com/example/a/beatbox_mvvm/BeatBoxFragment.java
@@ -0,0 +1,22 @@
+package com.example.a.beatbox_mvvm;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class BeatBoxFragment extends Fragment {
+    public static BeatBoxFragment newInstance() {
+        return new BeatBoxFragment();
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.fragment_beat_box, container, false);
+        return v;
+    }
+}
