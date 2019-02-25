ec5cc273aded7e89c60b2e041f7b02657d6cdec2
Your Name <you@example.com>
2019-02-25 17:22:23 +0900

    init

--- BeatBox_MVVM/app/src/main/java/com/example/a/beatbox_mvvm/BeatBoxFragment.java
+++ BeatBox_MVVM/app/src/main/java/com/example/a/beatbox_mvvm/BeatBoxFragment.java
@@ -1,12 +1,18 @@
 package com.example.a.beatbox_mvvm;
 
+import android.databinding.DataBindingUtil;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import com.example.a.beatbox_mvvm.databinding.FragmentBeatBoxBinding;
+import com.example.a.beatbox_mvvm.databinding.ListItemSoundBinding;
 
 public class BeatBoxFragment extends Fragment {
     public static BeatBoxFragment newInstance() {
@@ -16,7 +22,19 @@ public class BeatBoxFragment extends Fragment {
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        View v = inflater.inflate(R.layout.fragment_beat_box, container, false);
-        return v;
+        FragmentBeatBoxBinding binding = DataBindingUtil.inflate(
+                inflater, R.layout.fragment_beat_box, container, false);
+        binding.recyclerView.setLayoutManager(
+                new GridLayoutManager(getActivity(), 3) );
+        return binding.getRoot();
+    }
+
+    private class SoundHolder extends RecyclerView.ViewHolder{
+        private ListItemSoundBinding mBinding;
+
+        public SoundHolder(ListItemSoundBinding binding) {
+            super(binding.getRoot());
+            mBinding = binding;
+        }
     }
 }
