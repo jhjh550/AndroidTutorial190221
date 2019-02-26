be6b8648cd2bf4ba1c61a99ad5e0b9c85be3e9d6
Your Name <you@example.com>
2019-02-26 14:58:56 +0900

    photo gallery

--- PhotoGallery/app/src/main/java/com/example/a/photogallery/PhotoGalleryFragment.java
+++ PhotoGallery/app/src/main/java/com/example/a/photogallery/PhotoGalleryFragment.java
@@ -1,16 +1,20 @@
 package com.example.a.photogallery;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 public class PhotoGalleryFragment extends Fragment {
+    private static final String TAG = "PhotoGalleryFragment";
+
     private RecyclerView mRecyclerView;
 
     public static PhotoGalleryFragment newInstance() {
@@ -24,5 +28,19 @@ public class PhotoGalleryFragment extends Fragment {
         mRecyclerView = v.findViewById(R.id.fragment_photo_gallery_recycler_view);
         mRecyclerView.setLayoutManager(new GridLayoutManager(getActivity(), 3));
         return v;
+    }
+
+    private class FetchItemsTask extends AsyncTask<Void, Void, Void>{
+
+        @Override
+        protected Void doInBackground(Void... voids) {
+            try {
+                String result = new FlickrFetchr().getUrlString("https://www.google.com");
+                Log.d(TAG, "Fetched : "+result);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            return null;
+        }
     }
 }
