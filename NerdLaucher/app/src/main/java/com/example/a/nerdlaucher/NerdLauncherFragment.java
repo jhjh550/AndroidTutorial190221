28f4351e01e541845ae6bd8f72a5f7ff8a83fc2c
Your Name <you@example.com>
2019-02-26 13:57:24 +0900

    laucher

--- NerdLaucher/app/src/main/java/com/example/a/nerdlaucher/NerdLauncherFragment.java
+++ NerdLaucher/app/src/main/java/com/example/a/nerdlaucher/NerdLauncherFragment.java
@@ -2,6 +2,7 @@ package com.example.a.nerdlaucher;
 
 
 import android.content.Intent;
+import android.content.pm.ActivityInfo;
 import android.content.pm.PackageManager;
 import android.content.pm.ResolveInfo;
 import android.os.Bundle;
@@ -52,14 +53,18 @@ public class NerdLauncherFragment extends Fragment {
                 );
             }
         });
+        mRecyclerView.setAdapter(new ActivityAdapter(activities));
     }
 
-    private class ActivityHolder extends RecyclerView.ViewHolder{
+    private class ActivityHolder extends RecyclerView.ViewHolder
+                                    implements View.OnClickListener {
         private ResolveInfo mResolveInfo;
         private TextView mNameTextView;
 
         public ActivityHolder(@NonNull View itemView) {
             super(itemView);
+            mNameTextView = (TextView) itemView;
+            mNameTextView.setOnClickListener(this);
         }
 
         public void bindActivity(ResolveInfo resolveInfo){
@@ -67,6 +72,16 @@ public class NerdLauncherFragment extends Fragment {
             PackageManager pm = getActivity().getPackageManager();
             String appName = mResolveInfo.loadLabel(pm).toString();
             mNameTextView.setText(appName);
+        }
+
+        @Override
+        public void onClick(View v) {
+            ActivityInfo activityInfo = mResolveInfo.activityInfo;
+            Intent intent = new Intent(Intent.ACTION_MAIN);
+            intent.setClassName(
+                    activityInfo.applicationInfo.packageName,
+                    activityInfo.name);
+            startActivity(intent);
         }
     }
 
