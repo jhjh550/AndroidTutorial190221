7168d6cfdf92ecab28b7499fd3ff6a87e9ddb5fd
Your Name <you@example.com>
2019-02-26 16:25:21 +0900

    media player

--- MediaPlayer/app/src/main/java/com/example/a/mediaplayer/MainActivity.java
+++ MediaPlayer/app/src/main/java/com/example/a/mediaplayer/MainActivity.java
@@ -1,9 +1,11 @@
 package com.example.a.mediaplayer;
 
 import android.media.MediaPlayer;
+import android.os.Environment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.SeekBar;
 
 import java.io.IOException;
 
@@ -11,6 +13,7 @@ import java.io.IOException;
 public class MainActivity extends AppCompatActivity
                 implements View.OnClickListener {
     MediaPlayer mp = null;
+    SeekBar mSeekBar = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,6 +21,27 @@ public class MainActivity extends AppCompatActivity
         setContentView(R.layout.activity_main);
         findViewById(R.id.btnPlay).setOnClickListener(this);
         findViewById(R.id.btnStop).setOnClickListener(this);
+        mSeekBar = findViewById(R.id.seekBar);
+        mSeekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                if(fromUser){
+                    if(mp != null){
+                        mp.seekTo(progress);
+                    }
+                }
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
     }
 
     @Override
@@ -31,10 +55,25 @@ public class MainActivity extends AppCompatActivity
     private void musicPlay(){
         mp = new MediaPlayer();
         String path = "http://www.hochmuth.com/mp3/Haydn_Cello_Concerto_D-1.mp3";
+        String localPath = Environment.getExternalStorageDirectory()+"/Music/a.mp3";
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                while (mp != null){
+                   mSeekBar.setProgress(mp.getCurrentPosition());
+                    try {
+                        Thread.sleep(100);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        }).start();
         try {
             mp.setDataSource(path);
-            mp.prepare();
+            mp.prepareAsync();
             mp.start();
+            mSeekBar.setMax(mp.getDuration());
         } catch (IOException e) {
             e.printStackTrace();
         }
