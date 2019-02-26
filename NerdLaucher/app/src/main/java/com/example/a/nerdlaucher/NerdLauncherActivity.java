cd56ae119da2a09b75835591288dc992896ba76e
Your Name <you@example.com>
2019-02-26 12:20:53 +0900

    laucher

--- NerdLaucher/app/src/main/java/com/example/a/nerdlaucher/NerdLauncherActivity.java
+++ NerdLaucher/app/src/main/java/com/example/a/nerdlaucher/NerdLauncherActivity.java
@@ -4,10 +4,10 @@ import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class NerdLaucherActivity extends SingleFragmentActivity {
+public class NerdLauncherActivity extends SingleFragmentActivity {
 
     @Override
     protected Fragment createFragment() {
-        return null;
+        return NerdLauncherFragment.newInstance();
     }
 }
---
+++ NerdLaucher/app/src/main/java/com/example/a/nerdlaucher/NerdLauncherFragment.java
@@ -0,0 +1,56 @@
+package com.example.a.nerdlaucher;
+
+
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.content.pm.ResolveInfo;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+
+public class NerdLauncherFragment extends Fragment {
+    private RecyclerView mRecyclerView;
+    private static final String TAG = "NerdLauncher";
+
+    public static NerdLauncherFragment newInstance() {
+        return new NerdLauncherFragment();
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View v = inflater.inflate(R.layout.fragment_nerd_laucher, container, false);
+        mRecyclerView = v.findViewById(R.id.fragment_nerd_launcher_recycler_view);
+        mRecyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
+        setupAdapter();
+        return v;
+    }
+    private void setupAdapter(){
+        Intent startupIntent = new Intent(Intent.ACTION_MAIN);
+        startupIntent.addCategory(Intent.CATEGORY_LAUNCHER);
+
+        final PackageManager pm = getActivity().getPackageManager();
+        List<ResolveInfo> activities = pm.queryIntentActivities(startupIntent, 0);
+        Collections.sort(activities, new Comparator<ResolveInfo>() {
+            @Override
+            public int compare(ResolveInfo o1, ResolveInfo o2) {
+                return String.CASE_INSENSITIVE_ORDER.compare(
+                        o1.loadIcon(pm).toString(),
+                        o2.loadIcon(pm).toString()
+                );
+            }
+        });
+        Log.d(TAG, "activities : "+activities.size());
+    }
+
+}
