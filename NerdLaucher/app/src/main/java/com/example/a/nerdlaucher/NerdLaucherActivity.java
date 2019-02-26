f5d71e5cc76572399aab7b3bda2343769e2caade
Your Name <you@example.com>
2019-02-26 12:00:55 +0900

    laucher

---
+++ NerdLaucher/app/src/main/java/com/example/a/nerdlaucher/NerdLaucherActivity.java
@@ -0,0 +1,13 @@
+package com.example.a.nerdlaucher;
+
+import android.support.v4.app.Fragment;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class NerdLaucherActivity extends SingleFragmentActivity {
+
+    @Override
+    protected Fragment createFragment() {
+        return null;
+    }
+}
