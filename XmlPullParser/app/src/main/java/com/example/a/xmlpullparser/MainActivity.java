0f82e480edaefa4cb53c6355dd784cd89d5408c2
Your Name <you@example.com>
2019-02-26 11:23:25 +0900

    xmlPullParser

--- XmlPullParser/app/src/main/java/com/example/a/xmlpullparser/MainActivity.java
+++ XmlPullParser/app/src/main/java/com/example/a/xmlpullparser/MainActivity.java
@@ -1,15 +1,24 @@
 package com.example.a.xmlpullparser;
 
 import android.os.AsyncTask;
+import android.renderscript.Element;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 
+import org.xmlpull.v1.XmlPullParser;
+import org.xmlpull.v1.XmlPullParserException;
+import org.xmlpull.v1.XmlPullParserFactory;
+
+import java.net.URL;
 import java.util.ArrayList;
 
 // http://www.kma.go.kr/wid/queryDFSRSS.jsp?zone=1153051000
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
+    enum DataType{ none, hourType, dayType, tempType, wfKorType }
+     DataType type = DataType.none;
 
 
     class WeatherData{
@@ -42,8 +51,66 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     class MyPullParserTask extends AsyncTask<String, Void, String>{
 
         @Override
+        protected void onPostExecute(String s) {
+            super.onPostExecute(s);
+            Log.d("weather", s);
+        }
+
+        @Override
         protected String doInBackground(String... strings) {
-            return null;
+            String res = "";
+            try {
+                XmlPullParser xpp = XmlPullParserFactory.newInstance().newPullParser();
+                URL url = new URL(strings[0]);
+                xpp.setInput(url.openStream(), "utf-8");
+                int eventType = xpp.getEventType();
+                WeatherData data = null;
+                while(eventType != XmlPullParser.END_DOCUMENT){
+                    switch (eventType){
+                        case XmlPullParser.START_TAG:
+                            if(xpp.getName().equals("data")){
+                                data = new WeatherData();
+                                list.add(data);
+                            }else if(xpp.getName().equals("hour")){
+                                type = DataType.hourType;
+                            }else if(xpp.getName().equals("day")){
+                                type = DataType.dayType;
+                            }else if(xpp.getName().equals("temp")){
+                                type = DataType.tempType;
+                            }else if(xpp.getName().equals("wfKor")){
+                                type = DataType.wfKorType;
+                            }
+                            break;
+                        case XmlPullParser.TEXT:
+                            switch (type){
+                                case hourType:
+                                    data.hour = Integer.parseInt(xpp.getText());
+                                    break;
+                                case dayType:
+                                    data.day = Integer.parseInt(xpp.getText());
+                                    break;
+                                case tempType:
+                                    data.temp = Float.parseFloat(xpp.getText());
+                                    break;
+                                case wfKorType:
+                                    data.wfKor = xpp.getText();
+                                    break;
+                            }
+                            type = DataType.none;
+                            break;
+
+                    }
+                    eventType = xpp.next();
+                }
+
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+
+            for(WeatherData wd : list){
+                res += wd.toString()+"\n";
+            }
+            return res;
         }
     }
 }
